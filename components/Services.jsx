3c0e5f8eba439341b28c0f1909a7d34dd956d8e0
Francisco Cappelle <87731086+francappelle@users.noreply.github.com>
2022-05-31 11:11:55 +0100

    edited services descriptions

--- components/Services.jsx
+++ components/Services.jsx
@@ -15,7 +15,7 @@ const Services = (props) => {
             serviceUrl={
               "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
             }
-            description="I can build your digital presence."
+            description="I will help you to build a website that will be easy to use and responsive."
           />
 
           <ServicesItem
@@ -23,7 +23,7 @@ const Services = (props) => {
             serviceUrl={
               "https://images.unsplash.com/photo-1537498425277-c283d32ef9db?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1178"
             }
-            description="I can help you with your digital brand."
+            description="I will help you to manage your social media accounts and make them more effective."
           />
 
           <ServicesItem
@@ -31,7 +31,7 @@ const Services = (props) => {
             serviceUrl={
               "https://images.unsplash.com/photo-1548092372-0d1bd40894a3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
             }
-            description="I can help you organize your digital business."
+            description="I will help you to make your business more efficient and profitable."
           />
         </div>
       </div>
